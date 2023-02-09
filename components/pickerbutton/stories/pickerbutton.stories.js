23127b83503e1eb33e0643c6a4a3475ed170f8d8
Patrick Fulton <pfulton@adobe.com>
2023-02-09 10:08:34 -0500

    feat: wip pickerbutton stories

--- components/pickerbutton/stories/pickerbutton.stories.js
+++ components/pickerbutton/stories/pickerbutton.stories.js
@@ -1,6 +1,8 @@
 // Import the component markup template
 import { Template } from "./template";
 
+import { default as IconStories } from "@spectrum-css/icon/stories/icon.stories.js";
+
 export default {
   title: "Pickerbutton",
   description: "The Pickerbutton component is...",
@@ -16,10 +18,56 @@ export default {
       options: ["s", "m", "l", "xl"],
       control: "select"
     },
+    icon:
+      IconStories && IconStories.argTypes && IconStories.argTypes.iconName
+        ? IconStories.argTypes.iconName
+        : {},
+    label: {
+      name: "Label",
+      type: { name: "string" },
+      table: {
+        type: { summary: "string" },
+        category: "Component",
+      },
+      control: { type: "text" },
+    },
+    isInvalid: {
+      name: "Invalid styling",
+      type: { name: "boolean" },
+      table: {
+        type: { summary: "boolean" },
+        category: "Component",
+      },
+      control: "boolean",
+    },
+    isQuiet: {
+      name: "Quiet styling",
+      type: { name: "boolean" },
+      table: {
+        type: { summary: "boolean" },
+        category: "Component",
+      },
+      control: "boolean",
+    },
+    isDisabled: {
+      name: "Disabled",
+      type: { name: "boolean" },
+      table: {
+        type: { summary: "boolean" },
+        category: "State",
+      },
+      control: "boolean",
+    },
   },
   args: {
-    rootClass: "spectrum-",
+    rootClass: "spectrum-PickerButton",
+    customClasses: ["spectrum-PickerButton--right"],
     size: "m",
+    icon: "ChevronDown100",
+    isInvalid: false,
+    isQuiet: false,
+    isDisabled: false,
+    label: "All"
   },
   parameters: {
     actions: {
--- components/pickerbutton/stories/template.js
+++ components/pickerbutton/stories/template.js
@@ -2,19 +2,54 @@ import { html } from 'lit-html';
 import { classMap } from 'lit-html/directives/class-map.js';
 // import { ifDefined } from 'lit-html/directives/if-definedjs';
 
+import { Template as Icon } from "@spectrum-css/icon/stories/template.js";
+
 import "../index.css";
+
+// invalid, disabled, open, rounded, workflow icon only, ui icon only, texticon, quiet, sizes
+
+// spectrum-PickerButton--textuiicon
+// spectrum-PickerButton--uiicononly
+// spectrum-PickerButton--icononly
+// spectrum-PickerButton--rounded
+// is-open
+// icon size changes with t-shirt size
 
 export const Template = ({
   rootClass = "spectrum-PickerButton",
   customClasses = [],
+  icon,
+  label,
+  isInvalid = false,
+  isDisabled = false,
+  isQuiet = false,
+  hideLabel = false,
   size = "m",
-  // ...globals
+  ...globals
 }) => {
   return html`
-    <div class=${classMap({
-      [rootClass]: true,
-      [`${rootClass}--size${size?.toUpperCase()}`]: typeof size !== "undefined",
-      ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
-  })}></div>
+    <button
+      aria-haspopup="listbox"
+      class=${classMap({
+        [rootClass]: true,
+        [`${rootClass}--size${size?.toUpperCase()}`]: typeof size !== "undefined",
+        [`${rootClass}--quiet`]: isQuiet,
+        [`${rootClass}--invalid`]: isInvalid,
+        [`is-disabled`]: isDisabled,
+        ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
+      })}
+      ?disabled=${isDisabled}
+    >
+      <div class="spectrum-PickerButton-fill">
+        ${label && !hideLabel ? html`<span class="${rootClass}-label">${label}</span>` : ""}
+        ${icon
+          ? Icon({
+            ...globals,
+            iconName: icon,
+            customClasses: [`${rootClass}-icon`],
+          })
+        : ""}
+      </div>
+    </button>
   `;
 }
