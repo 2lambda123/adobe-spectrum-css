79051c77fa52eded3c3b8efcd7b9567f14b0a8fc
Patrick Fulton <pfulton@adobe.com>
2023-03-04 16:48:25 -0500

    chore(splitbutton): add story file and template

--- components/splitbutton/stories/splitbutton.stories.js
+++ components/splitbutton/stories/splitbutton.stories.js
@@ -1,6 +1,9 @@
 // Import the component markup template
 import { Template } from "./template";
 
+import { default as ButtonStories } from "@spectrum-css/button/stories/button.stories.js";
+import { default as IconStories } from "@spectrum-css/icon/stories/icon.stories.js";
+
 export default {
   title: "Splitbutton",
   description: "The Splitbutton component is...",
@@ -16,10 +19,21 @@ export default {
       options: ["s", "m", "l", "xl"],
       control: "select"
     },
+    variant: {
+      name: "Variant",
+      type: { name: "string", required: true },
+      table: {
+        type: { summary: "string" },
+        category: "Component",
+      },
+      options: ["cta", "primary", "secondary"],
+      control: "select",
+    }
   },
   args: {
-    rootClass: "spectrum-",
+    rootClass: "spectrum-SplitButton",
     size: "m",
+    variant: "cta",
   },
   parameters: {
     actions: {
--- components/splitbutton/stories/template.js
+++ components/splitbutton/stories/template.js
@@ -2,19 +2,74 @@ import { html } from 'lit-html';
 import { classMap } from 'lit-html/directives/class-map.js';
 // import { ifDefined } from 'lit-html/directives/if-defined.js';
 
+import { Template as Button } from '@spectrum-css/button/stories/template.js';
+import { Template as Icon } from "@spectrum-css/icon/stories/template.js";
+
 import "../index.css";
 
 export const Template = ({
   rootClass = "spectrum-SplitButton",
   customClasses = [],
   size = "m",
-  // ...globals
+  variant = "cta",
+  ...globals
 }) => {
   return html`
     <div class=${classMap({
       [rootClass]: true,
-      [`${rootClass}--size${size?.toUpperCase()}`]: typeof size !== "undefined",
       ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
-  })}></div>
+    })}>
+      ${Button({
+        ...globals,
+        variant: "accent",
+        size: "m",
+        treatment: "fill",
+        label: "Split Button",
+        customClasses: ["spectrum-SplitButton-action"]
+      })}
+      ${Button({
+        ...globals,
+        variant: "accent",
+        size: "m",
+        treatment: "fill",
+        hideLabel: true,
+        iconName: "ChevronDown100",
+        customClasses: ["spectrum-SplitButton-trigger"]
+      })}
+    </div>
+    
+    <br />
+    <br />
+    
+    <div class=${classMap({
+      [rootClass]: true,
+      [`${rootClass}--left`]: true,
+      ...customClasses.reduce((a, c) => ({ ...a, [c]: true }), {}),
+    })}>
+      ${Button({
+        ...globals,
+        variant: "accent",
+        size: "m",
+        treatment: "fill",
+        hideLabel: true,
+        iconName: "ChevronDown100",
+        customClasses: ["spectrum-SplitButton-trigger"]
+      })}
+      ${Button({
+        ...globals,
+        variant: "accent",
+        size: "m",
+        treatment: "fill",
+        label: "Split Button",
+        customClasses: ["spectrum-SplitButton-action"]
+      })}
+    </div>
   `;
 }
+
+{/* <button class="spectrum-Button spectrum-Button--sizeM spectrum-Button--fill spectrum-Button--accent spectrum-SplitButton-action"><span class="spectrum-Button-label">Split Button</span></button>
+  <button class="spectrum-Button spectrum-Button--sizeM spectrum-Button--fill spectrum-Button--accent spectrum-SplitButton-trigger">
+    <svg class="spectrum-Icon spectrum-UIIcon-ChevronDown100 spectrum-SplitButton-icon" focusable="false" aria-hidden="true">
+      <use xlink:href="#spectrum-css-icon-Chevron100" />
+    </svg>
+  </button> */}
