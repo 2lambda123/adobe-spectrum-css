fd87ff1fef1aadd3edeb6c4a4f33fb05404c31c6
[ Cassondra ] <castastrophe@users.noreply.github.com>
2023-12-06 15:06:48 -0500

    chore: hardening linter settings

--- plugins/stylelint-no-unknown-custom-properties/test.js
+++ plugins/stylelint-no-unknown-custom-properties/test.js
@@ -7,7 +7,7 @@ const stylelint = require("stylelint");
 const plugin = require("./index");
 const { ruleName, messages } = require("./index");
 
-async function compare(_, fixtureFilePath) {
+async function compare(t, fixtureFilePath) {
     const code = readFile(`./fixtures/${fixtureFilePath}`);
     return stylelint.lint({
         code,
@@ -24,17 +24,17 @@ function readFile(filename) {
     return fs.readFileSync(join(__dirname, filename), "utf8");
 }
 
-test("should throw an error for missing or extra parentheses in calc values", async (t) => {
-    const { results } = await compare(t, "missing-parenthesis.css");
+test("should throw an error for unknown custom properties", async (t) => {
+    const { results } = await compare(t, "unknown-prop.css");
 
     const warnings = results[0].warnings;
 
     t.is(warnings.length, 1);
     t.is(warnings[0].rule, ruleName);
-    t.is(warnings[0].text, messages.expected("calc(100px - 10px"));
+    t.is(warnings[0].text, messages.expected("--prefix-component-size"));
 });
 
-test(`should not throw an error for parenthesis usage`, async (t) => {
+test(`should not throw an error for unknown custom properties`, async (t) => {
     const { results } = await compare(t, "passing.css");
 
     const warnings = results[0].warnings;
